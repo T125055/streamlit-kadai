af5ca96f11cb1405962b81ad325437b50c61c387
T125055 <t125055@ed.sus.ac.jp>
2026-01-31 02:11:56 +0900

    1st commit

--- app.py
+++ app.py
@@ -5,19 +5,28 @@ st.title('自然公園の公園数と年間利用者数')
 
 df = pd.read_csv('park.csv')
 
+# 年次を数値化
 df['年次'] = pd.to_numeric(df['年次'], errors='coerce')
 df = df.dropna(subset=['年次'])
+
+# df が空なら止める
+if df.empty:
+    st.error('年次データが読み込めません。CSVを確認してください。')
+    st.stop()
+
 df['年次'] = df['年次'].astype(int)
 
 with st.sidebar:
     branch = st.multiselect('公園分類を選択してください（複数選択可）',
                             df['公園分類'].unique())
+    # 
     year = st.slider('年を選択してください',
                      min_value=int(df['年次'].min()),
                      max_value=int(df['年次'].max()),
                      value=int(df['年次'].min()),
                      step=1)
-    
+
+#     
 if branch:
     df = df[df['公園分類'].isin(branch)]
 
